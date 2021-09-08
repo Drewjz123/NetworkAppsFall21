3e6a52bb5ef4a6aab6adf2eaaefffc3edcd1d7d3
Dhruv Jani <dhruv.jani@rocketmail.com>
2021-09-08 19:22:45 -0400

    added a function

--- functions.js
+++ functions.js
@@ -12,3 +12,17 @@ const sumExp = function(a,b){
 
 console.log(sumExp(10,20));
 
+const sumArrow = (a,b) => { 
+ return  a + b;
+};
+console.log(sumArrow(6,10))
+//arrow function (ES6)
+
+//const name = (parameter list) =>  {// body of the function};
+
+
+//another way of writing this function 
+
+const sumSimplfied = (a,b) => a + b;
+
+console.log(sumSimplfied(10,15));
