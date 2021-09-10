204b70524398d9671608fdeeba92c0d18e77f599
Dhruv Jani <dhruv.jani@rocketmail.com>
2021-09-09 23:39:06 -0400

    Assignment file added and worked on

--- courses.js
+++ courses.js
@@ -21,31 +21,43 @@ const courses = [{
 ];
 
 //return course that matches the id
-function findById(id) {
-    return courses.find(course => course.id === id);
-}
+ function findById(id) {
+     return courses.find(course => course.id === id);
+ }
 
 //To do: implement save(course)
-
+function save(course){
+    courses.push(course);
+};
 
 //To do: implement findByPrefix(prefix)
+function findByPrefix(prefix){
+
+    return courses.filter(course => course.prefix == prefix)
+}
+
 
 
 
 //To do: implement updateById(id, course)
+function updateById(id, course){
 
+    
+
+}
 
 //To do: implement removeById(id)
 
 
 //To do: uncommet the following testing code when you are ready to test your functions
 
-// save({ prefix: 'ITIS', id: 5250, title: 'Computer forensics' });
-// save({ prefix: 'ITIS', id: 6220, title: 'Data privacy' });
-// save({ prefix: 'ITIS', id: 6420, title: 'Usable security and privacy' });
-// console.log(courses);
-// console.log(findById(5166));
-// console.log(findByPrefix('ITIS'));
+ save({ prefix: 'ITIS', id: 5250, title: 'Computer forensics' });
+
+ save({ prefix: 'ITIS', id: 6220, title: 'Data privacy' });
+ save({ prefix: 'ITIS', id: 6420, title: 'Usable security and privacy' });
+ //console.log(courses);
+ //console.log(findById(5166));
+console.log(findByPrefix('ITIS'));
 // console.log(removeById(6000));
 // console.log(updateById(6000));
 // console.log(updateById(5166, {
@@ -55,3 +67,4 @@ function findById(id) {
 // }, ));
 // console.log(removeById(6420));
 // console.log(courses);
+
