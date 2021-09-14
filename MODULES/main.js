678df709a1799e3f58e529c1fc34b7b4b0eacb27
Dhruv Jani <dhruv.jani@rocketmail.com@users.noreply.github.com>
2021-09-14 08:07:06 -0400

    Added how to require functions and export modules

--- MODULES/main.js
+++ MODULES/main.js
@@ -1,1 +1,9 @@
 const calculation = require('./temp/calculations');
+
+console.log(calculation);
+
+//console.log(calculation(2,3));
+
+//exports = module.exports; 
+
+console.log(calculation.add(2,3),calculation.sub(5,3));
--- MODULES/temp/calculations.js
+++ MODULES/temp/calculations.js
@@ -1,3 +1,23 @@
-function add(x,y){
-    return x+y;
-}
+// function add(x,y){
+//     return x+y;
+// }
+
+// function substract(x,y){
+//     return x - y;
+// }
+
+
+//module.exports = {}
+
+//module.exports.add = add; //where as here it will add a function to the object list and use the key add as a reference
+//exports.add = add;
+
+//module.exports = add; // Here the module.export will point to a function and not the object list;
+// exports.add = add; 
+// exports.sub = substract;
+
+
+//------- Trying to write in a shorter way
+
+exports.add = (x,y) => x + y ;
+exports.sub = (x,y) => x - y ;
