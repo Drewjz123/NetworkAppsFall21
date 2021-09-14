0e7bb7fdf5257ed92134749a68907d482d90381a
Dhruv Jani <dhruv.jani@rocketmail.com>
2021-09-13 22:52:47 -0400

    Added more files

---
+++ MODULES/main.js
@@ -0,0 +1,1 @@
+const calculation = require('./temp/calculations');
--- objects.js
+++ objects.js
@@ -1,17 +1,11 @@
 var dogs = {'name': 'bruno', 'type':'dog'};
 var cats = {'name': 'nancy', 'type': 'cat'};
 
-var pets = [{'name': 'bruno', 'type':'dog'},cats];
+var pets = [dogs,cats];
 pets.push({'name': 'slovena', 'type': 'cat'})
-//console.log(pets)
-//console.log(pets[2])
+console.log(pets)
+console.log(pets[2])
 
 cats.age = 2; 
 dogs['age'] = 14;
-//console.log(pets)
-
-pets[0].age = 14;
-pets[2].age = 20;
-pets[1].hungry = true;
 console.log(pets)
-console.log(cats)
